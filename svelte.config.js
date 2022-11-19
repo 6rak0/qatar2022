98272d04ec7d71c1a11ff3cdf0a699a3d9622760
6rak0 <ego.6rak0@gmail.com>
2022-11-19 15:58:00 -0600

    added adapter node, base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
+	},
+	paths: {
+		base: '/qatar2022'
 	},
 	preprocess: [
 		preprocess({
