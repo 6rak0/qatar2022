b69b3faadd514fb569a654e8873727a545249764
6rak0 <ego.6rak0@gmail.com>
2022-11-25 12:09:56 -0600

    hotfix

--- src/routes/partidos/semifinal/+page.svelte
+++ src/routes/partidos/semifinal/+page.svelte
@@ -1,20 +1,10 @@
 <script>
-	import { matches } from '$lib/stores';
 	import Match from '$lib/components/Match.svelte';
-	import Spinner from '$lib/components/Spinner.svelte'
 	export let data;
 </script>
 
 <section class="flex flex-wrap justify-center">
-	{#await $matches}
-		<Spinner />
-	{:then matches}
-		{#each matches as match}
-			{#if match.stage_name === 'Semi-final'}
-			<Match {match} link={data.link.data} />
-			{/if}
-		{/each}
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
+	{#each data.matches as match}
+		<Match {match} link={data.link.data} />
+	{/each}
 </section>
